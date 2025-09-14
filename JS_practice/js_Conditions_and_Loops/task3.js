69cb614efd12ba608cad26d70a541b8c4af37585
ntchnkk <katrusyanazarenko@gmail.com>
2025-09-14 11:18:09 -0400

    add folder js_Conditions_and_Loops with HW_5.1

---
+++ JS_practice/js_Conditions_and_Loops/task3.js
@@ -0,0 +1,17 @@
+const number = 5;
+
+// 3.1 for
+for (let i = 1; i <= 10; i++){
+    console.log(`${number} x ${i} = ${number * i}`)
+}
+
+console.log('\n'); //for readability
+
+
+// 3.2 while
+let j = 1;
+
+while (j <= 10) {
+    console.log(`${number} x ${j} = ${number * j}`);
+    j++;
+}
