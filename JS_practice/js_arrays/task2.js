ef7893f87550ea264dfe71d8c79cb6f56c694c4a
ntchnkk <katrusyanazarenko@gmail.com>
2025-09-21 21:20:25 -0400

    Add js_arrays folder with HW_8.1

---
+++ JS_practice/js_arrays/task2.js
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3, 4, 5];
+const newNumbers = numbers.map((num, i) => num * i);
+
+console.log("New array:", newNumbers);
---
+++ JS_practice/js_arrays/task5.js
@@ -0,0 +1,5 @@
+const firstArray = [1, 10, 14, 2];
+const secondArray = [4, 5, 43, 34];
+const array = [...firstArray, ...secondArray];
+
+console.log(array);
